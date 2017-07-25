1a11f97bf97aaf1dc8dd3b6e9d5b509cf1097681
Alex Chalk <alexchalk17@gmail.com>
2017-07-25 19:38:05 -0400

    Add increment-and-listify
    Function takes a vector containing a single integer, increments the
    integer, and returns it as a list.

--- test/arrow/core_test.clj
+++ test/arrow/core_test.clj
@@ -2,16 +2,7 @@
   (:require [midje.sweet :refer :all]
             [arrow.core :refer :all]))
 
-(println "You should expect to see three failures below.")
-
-(facts "about `first-element`"
-  (fact "it normally returns the first element"
-    (first-element [1 2 3] :default) => 1
-    (first-element '(1 2 3) :default) => 1)
-
-  ;; I'm a little unsure how Clojure types map onto the Lisp I'm used to.
-  (fact "default value is returned for empty sequences"
-    (first-element [] :default) => :default
-    (first-element '() :default) => :default
-    (first-element nil :default) => :default
-    (first-element (filter even? [1 3 5]) :default) => :default))
+(facts "about `increment-and-listify`"
+  (fact "takes a single element vector, increments the element, and returns it as a list"
+    (increment-and-listify [1]) => '(2)
+    (increment-and-listify [-1]) => '(0)))
